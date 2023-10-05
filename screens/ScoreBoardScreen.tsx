e1ad571deb058ec4529d08c810427bbecefae170
Grigelkotten <alexander.asplen@gmail.com>
2023-10-05 13:27:18 +0200

    Added ScoreBoard

--- screens/ScoreBoardScreen.tsx
+++ screens/ScoreBoardScreen.tsx
@@ -1,13 +1,34 @@
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Pressable} from 'react-native';
 import { RootStackParamList } from '../App';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import { useState } from 'react';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ScoreBoard'>;
 
 export default function DetailsScreen({navigation, route} : Props) {
+    const [firstCount, SetFirstCount] = useState(0);
+    const [secondCount, SetSecondCount] = useState(0);
+
+    const resetScores = () => {
+        SetFirstCount(0);
+        SetSecondCount(0);
+    }
+    
     return (
       <View style={styles.container}>
-        <Text>Score Board</Text>
+        <View style={styles.row}>
+        <Pressable onPress={() => SetFirstCount(firstCount + 1)}>
+            <Text style={styles.firstButton}>{firstCount}</Text>
+        </Pressable>
+        <Pressable onPress={() => SetSecondCount(secondCount + 1)}>
+            <Text style={styles.secondButton}>{secondCount}</Text>
+        </Pressable>
+        </View>
+        <View>
+            <Pressable onPress={resetScores}>
+                <Text style={styles.scoreButton}>Reset Scores</Text>
+            </Pressable>
+        </View>
       </View>
     );
   }
@@ -15,8 +36,45 @@ export default function DetailsScreen({navigation, route} : Props) {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+      gap: 5,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    row: {
+        justifyContent: 'space-between',
+        flexDirection: 'row', 
+        gap: 30
+    },
+    firstButton: {
+        backgroundColor: 'red',
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 90, 
+        padding: 20,
+        minWidth: 140,
+        minHeight: 220,
+        borderRadius: 20
+    }, 
+    secondButton: {
+        backgroundColor: 'blue',
+        color: 'white',
+        textAlign: 'center',
+        fontSize: 90,
+        padding: 20,
+        minWidth: 140,
+        minHeight: 220,
+        borderRadius: 20
+    },
+    scoreButton: {
+        backgroundColor: '#d0d4cf',
+        padding: 5,
+        borderStyle: 'solid',
+        borderColor: 'black',
+        borderWidth: 1,
+        textAlign: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30
+    }
   });
